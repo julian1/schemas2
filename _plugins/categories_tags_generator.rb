ab38f7013a62cb08405aa60b8efcfb951c789cbf
julian1 <julian.atkinson71@gmail.com>
2016-03-27 14:07:01 +1100

    scan dir

--- _plugins/categories_tags_generator.rb
+++ _plugins/categories_tags_generator.rb
@@ -1,35 +1,36 @@
 
 # http://geoexamples.com/other/2015/06/04/Jekyll-tags-plugin-gh-pages.html
-# module Jekyll
-#  class TagsGenerator < Generator
+module Jekyll
+ class TagsGenerator < Generator
 
-#    def generate(site)
-#        tags_dir = Dir.pwd + '/tags'
+   def generate(site)
+       tags_dir = Dir.pwd + '/tags'
 
-#        if !Dir.exists?(tags_dir)
-#            puts "Creating tags dir"
-#            Dir.mkdir(tags_dir)
-#        end
-#        regenerate_flag = false
+#       if !Dir.exists?(tags_dir)
+#           puts "Creating tags dir"
+#           Dir.mkdir(tags_dir)
+#       end
+#       regenerate_flag = false
 
-#        # TODO actually generating markdown, 
-#        # cleaner to create yaml in data, and can then use anywhere.
-#        site.tags.each do |i|
-#            if !File.exists?(tags_dir + '/' + i[0])
-#                puts "Creating tag page for: " + i[0]
-#                tag_file = File.new(tags_dir + '/' + i[0], "w")
-#                tag_file.puts("---\nlayout: about\ntag: " + i[0] + "\npermalink: /tags/" + i[0] + "/\n---")
-#                tag_file.close
+       # TODO actually generating markdown, 
+       # cleaner to create yaml in data, and can then use anywhere.
 
-#                regenerate_flag = true
-#            end
-#        end
+    puts "current dir #{ Dir.pwd }"
 
-#        if regenerate_flag
-#            FileUtils.touch Dir.pwd+'/_config.yml'
-#        end
+    Dir.glob(Dir.pwd + '/_posts/*.*').each do |f|
+      puts f
+    end
 
-#    end
-#  end
-# end
-#
+      site.posts.each  do |i|
+
+          puts "*** WHOOT Creating tag page for: " + i[0]
+       end
+
+#       if regenerate_flag
+#           FileUtils.touch Dir.pwd+'/_config.yml'
+#       end
+
+   end
+ end
+end
+
